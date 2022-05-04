9097f74c97173fbc5c79e42dc84da4598521df3e
Alex <a.clarke@runbox.com>
2022-05-04 11:18:29 +0100

    Token class

---
+++ src/dev/plasticzen/Token.java
@@ -0,0 +1,25 @@
+package dev.plasticzen;
+
+/*
+ * Scanner produces tokens which bundle substrings of the source code
+ * and other information to produce a token
+ */
+public class Token {
+    final TokenType type; // Enum identifier
+    final String lexeme; // Raw substring of source code
+    final Object literal; // Conversion of text to actual object (for literal string / num)
+    final int line; // Line in source this token was created
+
+    Token(TokenType type, String lexeme, Object literal, int line){
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+
+
+}
