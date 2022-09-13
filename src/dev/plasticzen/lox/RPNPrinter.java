431a3a522d79d6c29108296dddffb02a6c2fbd21
Alex <a.clarke@runbox.com>
2022-09-13 20:13:43 +0100

    Update RPNPrinter.java

--- src/dev/plasticzen/lox/RPNPrinter.java
+++ src/dev/plasticzen/lox/RPNPrinter.java
@@ -36,11 +36,15 @@ public class RPNPrinter implements Expr.Visitor<String>{
         // -123 45.67 *
 
         Expr expression = new Expr.Binary(
-                new Expr.Unary(
-                        new Token(TokenType.MINUS, "-", null, 1),
-                        new Expr.Literal(123)),
+                new Expr.Binary(
+                        new Expr.Literal(1),
+                        new Token(TokenType.PLUS, "+", null, 1),
+                        new Expr.Literal(2)),
                 new Token(TokenType.STAR, "*", null, 1),
-                new Expr.Literal(45.67));
+                new Expr.Binary(
+                        new Expr.Literal(4),
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(3)));
 
         System.out.println(new RPNPrinter().print(expression));
     }
